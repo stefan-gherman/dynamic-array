80b3323897ba92950c952196859a54b721fe9a55
Laszlo Molnar <laszlo.molnar@codecool.com>
2018-06-26 10:11:23 +0200

    toString method added to relevate overriding for second weekers

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,8 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
-
+    @Override
+    public String toString() {
+        return "Implement me plz.. Pretty plz..";
+    }
 }
